e5327ee4052a0987103b12cfe0245319f7fb785a
Andrei Conache <xpirt.dev@gmail.com>
2021-10-27 12:44:50 +0200

    fix: export for darwin only

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-var BrowserWindow = require('electron').BrowserWindow;
-var NativeExtension = require('bindings')('NativeExtension');
+var isMac = process.platform === 'darwin';
+var NativeExtension = isMac ? require('bindings')('NativeExtension') : undefined;
 
 module.exports = {
   makeKeyWindow: function(window) {
+    if (!isMac) return;
     return NativeExtension.MakeKeyWindow(window.getNativeWindowHandle());
   },
   makePanel: function(window) {
+    if (!isMac) return;
     return NativeExtension.MakePanel(window.getNativeWindowHandle());
   },
   makeWindow: function(window) {
+    if (!isMac) return;
     return NativeExtension.MakeWindow(window.getNativeWindowHandle());
   }
 }
